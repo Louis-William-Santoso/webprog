9f8b135a53872b5552f2d598c4ecc301f87c172f
louthod <louiswilli_san@outlook.com>
2026-04-18 20:23:13 +0700

    [TEST FITUR]coba2 download

--- index.php
+++ index.php
@@ -4,25 +4,48 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <style>
+        th, td{
+            border: 1px solid black;
+        }
+    </style>
 </head>
 <body>
     <nav>
         <a href="./setting.php">Settings</a>
         <a href="./tambah.php">Tambah</a>
     </nav>
-
-    <ul>
     <?php 
     if(isset($_COOKIE['transaksi'])){
+        echo "<table style='table-layout: fixed;'>
+                <tr>
+                    <th>no</th>
+                    <th>Tanggal</th>
+                    <th>Nominal</th>
+                </tr>";
+        $strData = "no,Tanggal, nominal\n";
+        $index = 0;
         $data = json_decode($_COOKIE['transaksi']);
         foreach($data as $key => $val){
-            echo "<li>tgl:{$key} nom:{$val}</li>";
+            $index +=1 ;
+            echo "<tr>
+                    <td>$index</td>
+                    <td>$key</td> 
+                    <td>$val</td>
+                  </tr>";
+
+            $strData = $strData . "$index,$key,$val\n";
         }
+        file_put_contents('tableData.csv',$strData);
+        echo "<tr>
+                <th><a href='tableData.csv' download='Data-Transaksi.csv'>tess Download table</a></th>
+            </tr>
+             </table>";
+
        
     }else{
         echo "Data kosong";
     }
     ?>
-    </ul>
 </body>
 </html>
--- setting.php
+++ setting.php
@@ -6,6 +6,18 @@
     <title>Document</title>
 </head>
 <body>
-    tess
+    <form action="" method="post">
+        <label>Urut dari:</label>
+        <input type="radio" name="urutDari" value="tgl" onchange="this.form.submit()">
+        <label for="tgl">Tanggal</label>
+        <input type="radio" name="urutDari" value="nom" onchange="this.form.submit()">
+        <label for=""nom>Nominal</label>
+    </form>
+
+    <?php
+    if(isset($_POST['urutDari'])){
+        var_dump($_POST['urutDari']);
+    }
+    ?>
 </body>
 </html>
